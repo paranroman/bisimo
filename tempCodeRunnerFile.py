b42b1c144a648030c76fbcf3d837ff5cc92159ff
Alifakbar99 <alifakbarharahap@gmail.com>
2026-01-31 23:13:24 +0700

    final

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,41 @@
+# Paul Ekman emotions database
+emotions = {
+    'happiness': {
+        'keywords': ['senang', 'bahagia', 'gembira', 'suka', 'asik', 'seru', 'asyik', 
+                     'girang', 'riang', 'ceria', 'tertawa', 'ketawa', 'lucu', 'haha', 
+                     'yeay', 'yes', 'wow keren', 'amazing', 'excited'],
+        'emoji': '😊',
+        'label': 'senang'
+    },
+    'sadness': {
+        'keywords': ['sedih', 'nangis', 'galau', 'kecewa', 'bete', 'down', 'murung', 
+                     'duka', 'kehilangan', 'rindu', 'sepi', 'sendiri', 'kesepian', 
+                     'susah', 'sulit', 'menangis'],
+        'emoji': '😢',
+        'label': 'sedih'
+    },
+    'anger': {
+        'keywords': ['marah', 'kesel', 'sebel', 'jengkel', 'benci', 'geram', 'dongkol', 
+                     'ngamuk', 'emosi', 'kesal', 'annoying', 'menyebalkan', 'bete', 'sewot'],
+        'emoji': '😠',
+        'label': 'kesal'
+    },
+    'fear': {
+        'keywords': ['takut', 'ngeri', 'seram', 'cemas', 'khawatir', 'was-was', 'panik', 
+                     'deg-degan', 'nervous', 'gelisah', 'tegang', 'anxiety'],
+        'emoji': '😨',
+        'label': 'takut'
+    },
+    'surprise': {
+        'keywords': ['kaget', 'terkejut', 'surprise', 'wow', 'waduh', 'gila', 'astaga', 
+                     'shock', 'tiba-tiba', 'mendadak', 'gak nyangka', 'unexpected'],
+        'emoji': '😮',
+        'label': 'kaget'
+    },
+    'disgust': {
+        'keywords': ['jijik', 'mual', 'eneg', 'geli', 'ih', 'eww', 'jorok', 'kotor', 
+                     'najis', 'gak suka'],
+        'emoji': '🤢',
+        'label': 'jijik'
+    }
+}
